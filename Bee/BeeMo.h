41d2c3755d6da5ba4edf52cb47a568c42929e51f
Derrick Ho <derrickho328@gmail.com>
2016-06-05 15:42:30 -0400

    beam

---
+++ Bee/BeeMo.h
@@ -0,0 +1,13 @@
+//
+//  BeeMo.h
+//  Bee
+//
+//  Created by Derrick  Ho on 6/5/16.
+//  Copyright © 2016 Derrick  Ho. All rights reserved.
+//
+
+@import BeeMo
+
+@interface BeeMo : UIViewConroller
+
+@end
---
+++ Bee/BeeMo.m
@@ -0,0 +1,13 @@
+//
+//  Bee.m
+//  Bee
+//
+//  Created by Derrick  Ho on 6/5/16.
+//  Copyright © 2016 Derrick  Ho. All rights reserved.
+//
+
+#import "Bee.h"
+
+@implementation Bee
+
+@end
