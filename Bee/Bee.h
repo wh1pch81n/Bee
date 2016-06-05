b5f3d365ed609fc3a7165f32b722ecb4052f1135
Derrick Ho <derrickho328@gmail.com>
2016-06-05 15:39:00 -0400

    Initial Commit

---
+++ Bee/Bee.h
@@ -0,0 +1,19 @@
+//
+//  Bee.h
+//  Bee
+//
+//  Created by Derrick  Ho on 6/5/16.
+//  Copyright © 2016 Derrick  Ho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bee.
+FOUNDATION_EXPORT double BeeVersionNumber;
+
+//! Project version string for Bee.
+FOUNDATION_EXPORT const unsigned char BeeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bee/PublicHeader.h>
+
+
